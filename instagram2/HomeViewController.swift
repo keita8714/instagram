736298a2cddb2ef2cb2b6849ca8047e4f0c0d18a
mk <munekawa0807@outlook.jp>
2020-07-25 20:36:05 +0900

    変更

---
+++ instagram2/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  instagram2
+//
+//  Created by Keita Munekawa on 2020/07/11.
+//  Copyright © 2020 KM. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
